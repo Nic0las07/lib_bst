39db45ac1d19fb97e9d8a45e5dcb4e3ee78f521f
Nic0las07 <156135578+Nic0las07@users.noreply.github.com>
2024-12-09 19:05:50 +0100

    Update binary_tree.h

--- binary_tree.h
+++ binary_tree.h
@@ -7,15 +7,15 @@ class Node {
 private:
 	int data;
 	size_t weight;
-	Node* left;
-	Node* right;
+	Node* lchild;
+	Node* rchild;
 public:
-	Node(int value);
-	int getData();
-	Node* getLeftChild();
-	Node* getRightChild();
+	Node(int value = 0);
+	int& getData();
+	Node* getLchild();
+	Node* getRchild();
+	Node* insertR(int k);
 	Node* insertI(int k);
-	void insertR(int k);
 	bool searchI(int k);
 	bool searchR(int k);
 	Node* findMin(Node* root);
@@ -26,7 +26,7 @@ public:
 	int BST_height();
 	bool isBst(int max = INT_MAX, int min = INT_MIN);
 	friend std::ostream& operator<<(std::ostream& os, Node& root);
-	friend std::istream& operator>>(std::istream& is, Node& root);
+	friend std::istream& operator>>(std::istream& is, Node*& root);
 };
 
 #endif
